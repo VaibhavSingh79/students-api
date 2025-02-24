37e53d65b3619e6a8fa99aae1dcd1da8132f7a06
Vaibhav Singh <E21CSEU0675@bennett.edu.in>
2025-02-24 19:13:26 +0530

    parse config files

---
+++ internal/config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type HTTPServer struct {
+	Addr string
+}
+
+// env-default:"production"
+type Config struct {
+	ENV         string `yaml:"env" env:"ENV" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+func MustLoad() *Config {
+	var configPath string
+
+	configPath = os.Getenv("CONFIG_PATH") // get config path from env, os.Getenv is used to get the value of the environment variable.
+
+	if configPath == "" {
+		flags := flag.String("config", "", "path to the config file ") // flag.String is used to define a string flag with specified name, default value, and usage string.
+		flag.Parse()                                                   // flag.Parse is used to parse the command-line flags.
+
+		configPath = *flags // get config path from flag.
+
+		if configPath == "" {
+			log.Fatal("Config path is not set") // log.Fatal is used to log the error message and exit the program.
+		}
+	}
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath) //fatalf is used to log the error message and exit the program.
+	}
+
+	var cfg Config
+
+	err := cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		log.Fatalf("cannot read config file: %s", err.Error())
+	}
+	return &cfg
+}
